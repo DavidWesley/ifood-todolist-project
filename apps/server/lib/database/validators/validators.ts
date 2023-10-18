4a0e91813141403c5658b8c2a376159b8da98958
David Wesley <contato.dwesley@outlook.com>
2023-10-18 20:09:37 -0300

    feat(database): Add commons validators
    add validators for name, UUID, email, datetime, boolean, number, and array

---
+++ apps/server/lib/database/validators/validators.ts
@@ -0,0 +1,13 @@
+import { DateTimeString } from "../in-memory-table"
+
+export type ValidatorFunction = (value: string) => boolean
+
+export const Validators = Object.freeze({
+    isValidName: (name: string): boolean => name.length >= 1 && name.length <= 100,
+    isUUID: (id: string): boolean => /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(id),
+    isEmail: (email: string): boolean => /^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i.test(email),
+    isDateTimeString: (date: string | DateTimeString): boolean => /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/.test(date),
+    isBoolean: (value: string | boolean): boolean => value === "true" || value === "false" || value === false || value === true,
+    isNumber: (value: string | number): boolean => /^-?\d+$/.test(value.toString(10)),
+    isArray: (value: string): boolean => Array.isArray(JSON.parse(value)),
+}) satisfies Readonly<Record<string, ValidatorFunction>>
