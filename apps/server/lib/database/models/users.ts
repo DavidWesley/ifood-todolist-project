d8be3ac1c86f2222357f204ea3f313f40096c5c6
David Wesley <contato.dwesley@outlook.com>
2023-10-18 20:12:50 -0300

    feat(database): Add users entity
    add UserModel and  users table model and validators

---
+++ apps/server/lib/database/models/users.ts
@@ -0,0 +1,24 @@
+import { InMemoryTable, InMemoryTableModel } from "../in-memory-table"
+import { ValidatorFunction, Validators } from "../validators/validators"
+
+export interface UserModel extends Partial<InMemoryTableModel> {
+    username: string
+    email: string
+    password: string
+}
+
+const MIN_USERNAME_LENGTH = 5
+const MAX_USERNAME_LENGTH = 20
+
+const UserValidators = Object.freeze({
+    isPassword: (value: string): boolean => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,32}$/.test(value),
+    isValidUsername: (value: string): boolean => value.length >= MIN_USERNAME_LENGTH && value.length <= MAX_USERNAME_LENGTH && /^[a-zA-Z0-9]+$/.test(value),
+}) satisfies Readonly<Record<string, ValidatorFunction>>
+
+export const users = new InMemoryTable<UserModel>("users", {
+    columns: [
+        ["username", { validators: [Validators.isValidName, UserValidators.isValidUsername] }],
+        ["email", { validators: [Validators.isEmail] }],
+        ["password", { validators: [UserValidators.isPassword] }],
+    ],
+})
