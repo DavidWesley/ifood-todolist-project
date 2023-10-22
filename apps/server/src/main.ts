fa12e0a6c2979e328b0073c0dead6bd6019ae119
David Wesley <contato.dwesley@outlook.com>
2023-10-22 14:56:17 -0300

    feat(server): create server
    Add logger, CORS, and rate limit to server configuration

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -1,2 +1,17 @@
+import { ENV } from "@/lib/env.ts"
+import { server } from "@/server.ts"
 
-console.log("Starting server")
+server.listen(
+    {
+        port: ENV.PORT ?? 3333,
+        host: ENV.NODE_ENV === "production" ? "0.0.0.0" : "localhost",
+    },
+    (err, address) => {
+        if (err) {
+            console.error(err)
+            process.exit(1)
+        }
+
+        console.info(`server listening on ${address}`)
+    }
+)
