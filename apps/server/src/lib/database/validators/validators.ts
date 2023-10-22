93ea7663dec54d120d4f81d5c21562c34de5bb03
David Wesley <contato.dwesley@outlook.com>
2023-10-22 08:56:40 -0300

    refactor: Improve type safety in Validators module

--- apps/server/src/lib/database/validators/validators.ts
+++ apps/server/src/lib/database/validators/validators.ts
@@ -1,13 +1,13 @@
-import { DateTimeString } from "../in-memory-table.ts"
+import { DateTimeString, UUID } from "../in-memory-table.ts"
 
 export type ValidatorFunction = (value: string) => boolean
 
 export const Validators = Object.freeze({
     isValidName: (name: string): boolean => name.length >= 1 && name.length <= 100,
-    isUUID: (id: string): boolean => /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(id),
+    isUUID: (id: string): id is UUID => /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(id),
     isEmail: (email: string): boolean => /^(?!\.)(?!.*\.\.)([A-Z0-9_+-.]*)[A-Z0-9_+-]@([A-Z0-9][A-Z0-9-]*\.)+[A-Z]{2,}$/i.test(email),
-    isDateTimeString: (date: string | DateTimeString): boolean => /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/.test(date),
-    isBoolean: (value: string | boolean): boolean => value === "true" || value === "false" || value === false || value === true,
-    isNumber: (value: string | number): boolean => /^-?\d+$/.test(value.toString(10)),
+    isDateTimeString: (date: string | DateTimeString): date is DateTimeString => /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z/.test(date),
+    isBoolean: (value: string | boolean): value is boolean => value === "true" || value === "false" || value === false || value === true,
+    isNumber: (value: string | number): value is number => /^-?\d+$/.test(value.toString(10)),
     isArray: (value: string): boolean => Array.isArray(JSON.parse(value)),
 }) satisfies Readonly<Record<string, ValidatorFunction>>
