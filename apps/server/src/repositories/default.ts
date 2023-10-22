490fffbc44a3dc97573e3f54df58a6c07face062
David Wesley <contato.dwesley@outlook.com>
2023-10-22 15:01:21 -0300

    feat(server/repositories): add default repository implementation

---
+++ apps/server/src/repositories/default.ts
@@ -0,0 +1,13 @@
+import { InMemoryModelQuery, UUID } from "@/lib/database/in-memory-table.ts"
+
+export interface Repository<M> {
+    create(data: M): Promise<UUID | undefined>
+    findAll(): Promise<M[]>
+    findById(id: UUID): Promise<M | null>
+    findOne(filters: InMemoryModelQuery<M>["filters"]): Promise<M | null>
+    update(id: UUID, data: Partial<M>): Promise<void>
+    updateMany(filters: InMemoryModelQuery<M>["filters"], data: Partial<M>): Promise<number>
+    delete(id: UUID): Promise<boolean>
+    deleteMany(filters: InMemoryModelQuery<M>["filters"]): Promise<number>
+    count(filters: InMemoryModelQuery<M>["filters"]): Promise<number>
+}
