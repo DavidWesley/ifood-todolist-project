01106b17a4565a9a730da231a258acafbfecb177
dyda91 <edjamasalmeida@outlook.com>
2023-10-26 02:06:19 -0300

    git: resolvendo conflito de merge entre as branches

--- apps/web/src/components/cadastro.tsx
+++ apps/web/src/components/cadastro.tsx
@@ -1,33 +1,33 @@
 import axios from 'axios';
 import { useEffect } from 'react';
 
-export let usuarioId = null; 
+export let usuarioId = null;
 
 const CadastroUsuario = () => {
-  async function handleCadastroUsuario() {
-    try {
-      const response = await axios.post('http://127.0.0.1:3333/auth/register', {
-        username: 'edjalma',
-        email: 'dyda91@gmail.com',
-        password: 'dida1991'
-      });
+    async function handleCadastroUsuario() {
+        try {
+            const response = await axios.post('http://127.0.0.1:3333/auth/register', {
+                username: 'edjalma',
+                email: 'dyda91@gmail.com',
+                password: 'dida1991'
+            });
 
-      if (response.status === 201) {
-        usuarioId = response.data.id; 
-      } else {
-        console.error('Falha no cadastro do usuário. Status:', response.status);
-      }
-    } catch (error) {
-      console.error('Erro ao cadastrar usuário:', error);
+            if (response.status === 201) {
+                usuarioId = response.data.id;
+            } else {
+                console.error('Falha no cadastro do usuário. Status:', response.status);
+            }
+        } catch (error) {
+            console.error('Erro ao cadastrar usuário:', error);
+        }
     }
-  }
 
 
-  useEffect(() => {
-    handleCadastroUsuario();
-  }, []);
+    useEffect(() => {
+        handleCadastroUsuario();
+    }, []);
 
- 
+
 }
 
 export default CadastroUsuario;
