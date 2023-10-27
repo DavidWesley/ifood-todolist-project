7014d398306fa9f6d475a1e8aff1b53c32b8a669
dyda91 <edjamasalmeida@outlook.com>
2023-10-27 01:11:47 -0300

    feat(web): adicionar integração com servidor

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,19 +1,17 @@
+import axios from 'axios';
 import React, { useEffect, useRef, useState } from 'react';
 import { ThemeProvider } from 'styled-components';
-// import { v4 as uuidv4 } from 'uuid';
-import axios from 'axios';
 import Header from './components/Header';
 import TodoFilters from './components/TodoFilters';
 import TodoFooter from './components/TodoFooter';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
-import CadastrarUsuario from './components/cadastro';
+import CadastrarUsuario, { usuarioId } from './components/cadastro';
 import { StyledBanner } from './styled/Banner.styled';
 import GlobalStyles from './styled/Global';
 import { StyledMain } from './styled/Main.styled';
 import { darkTheme, lightTheme } from './themes';
-
-const LOCAL_STORAGE_KEY = 'todoApp.todos';
+;
 
 interface Todo {
     todoName: string,
@@ -41,6 +39,7 @@ const App: React.FC<Todo> = () => {
     const moon = `<svg xmlns="http://www.w3.org/2000/svg" width="26" height="26"><path fill="#FFF" fillRule="evenodd" d="M13 0c.81 0 1.603.074 2.373.216C10.593 1.199 7 5.43 7 10.5 7 16.299 11.701 21 17.5 21c2.996 0 5.7-1.255 7.613-3.268C23.22 22.572 18.51 26 13 26 5.82 26 0 20.18 0 13S5.82 0 13 0z"/></svg>`;
 
     const [colorTheme, setColorTheme] = useState<string>('light');
+
 
     useEffect(() => {
         const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '[]') as Todo[];
