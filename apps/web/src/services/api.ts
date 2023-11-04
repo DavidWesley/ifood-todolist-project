9999f1224e98c28246dea3c11ab8f72c8c3068be
David Wesley <contato.dwesley@outlook.com>
2023-11-04 20:06:21 -0300

    fix(web): organize files, components, functions, services and folders
    - Added new assets: icon.svg, TodoIcon.svg
    - Added new routes: routes.ts
    - Added new services: api.ts
    - Added new components: Layout, Page
    - Added new functions: isRunningVite, isSafeContext

---
+++ apps/web/src/services/api.ts
@@ -0,0 +1,3 @@
+import { getAPIClient } from "@/libs/axios"
+
+export const apiClient = getAPIClient()
---
+++ apps/web/src/utils/index.ts
@@ -0,0 +1,17 @@
+export function isRunningVite() {
+    const isViteContext = import.meta.env["VITE_CONTEXT"] ? true : false
+    const hasSomeViteEnvVariables = Object.keys(import.meta.env).some((k) => k.startsWith("VITE_"))
+
+    return isViteContext || hasSomeViteEnvVariables
+}
+
+export function isSafeContext() {
+    if (window.SharedArrayBuffer) {
+        console.info("Supports to SharedArrayBuffer is active in this context")
+        return true
+    } else {
+        console.warn("browser does not support SharedArrayBuffer")
+        console.info("Maybe this is not a secure context")
+        return false
+    }
+}
